7b58f90f2a434a1d5015cd87948a3b4adeadf9c8
Pavan Kumar Jadda <17564080+pavankjadda@users.noreply.github.com>
2022-06-28 10:28:42 -0400

    feat(ngrx): adds proxy.conf.json

--- apps/ngrx-demo/src/app/employee/employee-data.service.ts
+++ apps/ngrx-demo/src/app/employee/employee-data.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DefaultDataService, HttpUrlGenerator, Logger } from '@ngrx/data';
 
-import { Observable } from 'rxjs';
 import { Employee } from './employee';
 
 @Injectable()
@@ -10,10 +9,5 @@ export class EmployeeDataService extends DefaultDataService<Employee> {
 	constructor(http: HttpClient, httpUrlGenerator: HttpUrlGenerator, logger: Logger) {
 		super('Employee', http, httpUrlGenerator);
 		logger.log('Created custom Employee EntityDataService');
-	}
-
-	override getAll(): Observable<Employee[]> {
-		console.log('Inside EmployeeDataService.getAll() method');
-		return super.getAll();
 	}
 }
